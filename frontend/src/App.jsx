13e92c348f53984db5be7df66e29bb7b0c0c6445
fadhel_ben <118136846+fadhelbenosmtane@users.noreply.github.com>
2024-02-10 09:10:55 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import './App.css'
-import HomePage from './components/home page/HomePage'
+import './App.css';
+import MyEvent from './components/home page/MyEvent';
+import HomePage from './components/home page/HomePage';
+import NotificationPage from './components/home page/Notification';
+import { BrowserRouter, Routes,Route } from 'react-router-dom'; // Use Route instead of Routes
+import Layout from './components/home page/Layout';
+import ReactDOM from "react-dom/client";
+
+
 
 function App() {
-
   return (
     <>
-      <HomePage/>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/my-event" element={<MyEvent />} /> 
+        <Route path="/notification" element={<NotificationPage />} />
+        </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
